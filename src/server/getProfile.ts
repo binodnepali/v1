5992353ab0fd4523bd2df978b34f28398622a9c8
Binod Nepali <nepalibinod9@gmail.com>
2024-04-29 16:24:18 +0200

    chore: add log to check api url

--- src/server/getProfile.ts
+++ src/server/getProfile.ts
@@ -3,6 +3,8 @@ import { Profile } from "../types/Profile.ts";
 export const getProfile = async () => {
   const baseurl = Deno.env.get("BASE_URL") || "http://localhost:8000/api";
 
+  console.log("baseurl", baseurl);
+
   const response = await fetch(`${baseurl}/profile`);
   return await response.json() as Profile;
 };
